1c2412cf2493bab9eb5f629ef4aba1d1fedfcd69
Renato Luna <renatoluna@gmail.com>
2014-11-27 00:19:40 -0200

    Atualizando timer

--- src/scripts/clock.js
+++ src/scripts/clock.js
@@ -2,20 +2,27 @@ var script = document.createElement('script');
 script.src = 'cabrito.js';
 document.body.appendChild(script);
 var clock = cabrito.Class.extends(function () {
+    var status = 0;
     this.date = function () {
         var d = new Date();
         return d;
     };
     this.pomodoro = function () {
+        var d1 = this.date();
         var d2 = this.date();
         d2.setMinutes(d2.getMinutes() + 25);
         var int = setInterval(function () {
-            var d = new Date();
-            if (d >= d2){
+            status += 1;
+            if (d1 >= d2){
+                status = 0;
                 clearInterval(int);
                 alert('POMODORO!');
             }
         },1000);
     };
+    this.status = function () {
+        return status;
+    };
 });
-clock.pomodoro();
+var timer = clock.create();
+timer.pomodoro();
