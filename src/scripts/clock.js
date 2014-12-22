51905196eb1385afbe976ebee3940362d82012b4
Renato Luna <renatoluna@gmail.com>
2014-12-22 16:55:32 -0200

    Criando relógio

--- src/scripts/clock.js
+++ src/scripts/clock.js
@@ -1,28 +1,53 @@
-var script = document.createElement('script');
-script.src = 'cabrito.js';
-document.body.appendChild(script);
-var clock = cabrito.Class.extends(function () {
+var clock = cabrito.Class.extends(function (time) {
     var status = 0;
+    this.timeElapsed = time;
     this.date = function () {
         var d = new Date();
         return d;
     };
-    this.pomodoro = function () {
+
+    var getMinutes = function (seconds_left) {
+        return parseInt(seconds_left / 60);
+    };
+
+    var getSeconds = function (seconds_left) {
+        var seconds = parseInt(seconds_left % 60);
+        if (seconds <= 9) {
+            seconds = '0' + seconds;
+        }
+        return seconds;
+    };
+
+    this.getTimer = function (seconds_left) {
+        return getMinutes(seconds_left) + ':' + getSeconds(seconds_left);
+    };
+
+    this.pomodoro = function (time, div) {
         var d1 = this.date();
         var d2 = this.date();
-        d2.setMinutes(d2.getMinutes() + 25);
+        d2.setMinutes(d2.getMinutes() + time);
+        var seconds_left = (d2 - d1) / 1000;
+        div = document.getElementById(div);
+        if (div) {
+            div.innerHTML = this.getTimer(seconds_left);
+        }
+        var getTimer = this.getTimer;
         var int = setInterval(function () {
             status += 1;
-            if (d1 >= d2){
+            seconds_left = seconds_left - 1;
+            if (div) {
+                div.innerHTML = getTimer(seconds_left);
+            }
+            if (d1 >= d2) {
                 status = 0;
                 clearInterval(int);
                 alert('POMODORO!');
             }
-        },1000);
+        }, 1000);
     };
     this.status = function () {
         return status;
     };
 });
-var timer = clock.create();
-timer.pomodoro();
+// var timer = clock.create();
+// timer.pomodoro(25, 'timer');
