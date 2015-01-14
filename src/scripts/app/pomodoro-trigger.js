1d73dd39b625db11d2b0e6e82f1ddf5b7ec3d39b
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2015-01-14 14:26:31 -0200

    Tratamento do botão do Pomodoro

--- src/scripts/app/pomodoro-trigger.js
+++ src/scripts/app/pomodoro-trigger.js
@@ -3,6 +3,7 @@ cabrito.PageLoad.elementReady('timer', function () {
     var timer = clock.create();
 
     cabrito.PageLoad.elementReady('pomodoro-trigger', function () {
+        document.getElementById('pomodoro-trigger').removeAttribute('href');
         document.getElementById('pomodoro-trigger').addEventListener('click', function () {
             timer.pomodoro(25, 'timer');
         });
