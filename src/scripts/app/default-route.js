b1d76e9e4b7e47ceb28e5ead41cf7f8a909d906b
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2015-01-26 11:15:27 -0200

    Testando Redirecionamento caso o usuário já tenha entrado na página

--- src/scripts/app/default-route.js
+++ src/scripts/app/default-route.js
@@ -1,13 +1,17 @@
 (function () {
     var route = cabrito.Class.extends(function () {
         this.knowTechnique = function () {
-            if (cabrito.AB.cookieToggle('about', '1')) {
-                document.cookie = 'about=2';
-                window.location.href = '/pomodoro.html'
-            }
             if (cabrito.AB.cookieToggle('about', '0')) {
+                utils.setCookie('about', '1', 7, '.ponto4u.com.br');
+                return;
+            }
+            if (cabrito.AB.cookieToggle('about', '1')) {
+                utils.setCookie('about', '0', 7, '.ponto4u.com.br');
+                window.location.href = '/pomodoro.html';
+                return;
+            } else {
                 setTimeout(function () {
-                    document.cookie = 'about=1';
+                    utils.setCookie('about', '1', 7, '.ponto4u.com.br');
                 }, 30000);
             }
         }
