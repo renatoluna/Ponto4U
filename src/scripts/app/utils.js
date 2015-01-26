112258c3c6a10aa15b7e2beb85e7abf768557aeb
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2015-01-26 11:14:35 -0200

    Criando método de escrita em Cookie

--- src/scripts/app/utils.js
+++ src/scripts/app/utils.js
@@ -14,5 +14,26 @@ var utils = cabrito.Class.extends(function (time) {
         }
 
         return unescape(cookie.substring(middleIndex, endIndex));
-    }
+    };
+
+    this.setCookie = function(name, value, expiredays, domain) {
+        var cookie;
+        if (!name) {
+            return;
+        }
+
+        expiredays = expiredays || 45;
+
+        if(value.substring(0,1) == ";") {
+            value = value.substr(1);
+        }
+        value = escape(value);
+
+        var exdate = exdate || new Date();
+        exdate.setDate(exdate.getDate() + expiredays);
+        exdate = exdate.toUTCString();
+        
+        cookie = name + "=" + value + ";expires=" + exdate + ";path=/;domain="+ domain +";";
+        return document.cookie = cookie;
+    };
 });
