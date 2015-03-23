9488b6ea079a902ddf02641c02610cbad050e734
Renato Luna <renatoluna@gmail.com>
2015-03-23 10:32:40 -0300

    Modificando ID do relógio

--- src/scripts/app/pomodoro-trigger.js
+++ src/scripts/app/pomodoro-trigger.js
@@ -1,21 +1,21 @@
-cabrito.PageLoad.elementReady('timer', function () {
+cabrito.PageLoad.elementReady('time-elapsed', function () {
 
     var timer = clock.create();
-    timer.pomodoro(25, 'timer');
+    timer.pomodoro(25, 'time-elapsed');
 
     cabrito.PageLoad.elementReady('pomodoro-trigger', function () {
         document.getElementById('pomodoro-trigger').removeAttribute('href');
         document.getElementById('pomodoro-trigger').addEventListener('click', function () {
-            timer.pomodoro(25, 'timer');
+            timer.pomodoro(25, 'time-elapsed');
         });
     });
 
     cabrito.PageLoad.elementReady('5-min-trigger', function () {
         document.getElementById('5-min-trigger').addEventListener('click', function () {
-            timer.pomodoro(5, 'timer');
+            timer.pomodoro(5, 'time-elapsed');
         });
         document.getElementById('pomodoro-interval').addEventListener('click', function () {
-            timer.pomodoro(5, 'timer');
+            timer.pomodoro(5, 'time-elapsed');
         });
     });
 });
